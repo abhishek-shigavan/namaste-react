ea31e5d9fe74a28c740012780a38e288c1671e70
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-07-13 12:06:24 +0530

    Add: Episode 1

---
+++ App.js
@@ -0,0 +1,32 @@
+ const heading1 = React.createElement("h1", {id : "heading"}, "Hello from React !!!");
+//createElement ==>  h1 => tag name, {} => tag attribute, "_______" => children / content 
+const root1 = ReactDOM.createRoot(document.getElementById("root"));
+//root1.render(heading1);
+
+/**
+ * 
+ * <div id="parent">
+ *   <div id="child1">
+ *     <h1>Im h1 tag</h1>
+ *     <h2>Im h2 tag</h2>
+ *   </div>
+ *   <div id="child2">
+ *     <h1>Im h1 tag</h1>
+ *     <h2>Im h2 tag</h2>
+ *   </div>
+ * </div>
+ * 
+ */
+
+const parent = React.createElement("div", {id : "parent"}, [ 
+  React.createElement("div", {id : "child1"}, [
+    React.createElement("h1", {}, "Im h1 tag"),
+    React.createElement("h2", {}, "Im h2 tag")
+  ]),
+  React.createElement("div", {id : "child2"}, [
+    React.createElement("h1", {}, "Im h1 tag"),
+    React.createElement("h2", {}, "Im h2 tag")
+  ]),
+])
+
+root1.render(parent)
