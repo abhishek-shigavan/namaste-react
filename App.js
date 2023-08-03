698ab59cdf02769b3a7392145cb54c0b77e86db9
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-03 12:31:33 +0530

    MODIFIED: imported React & ReactDOM

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
- const heading1 = React.createElement("h1", {id : "heading"}, "Hello from React !!!");
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const heading1 = React.createElement("h1", {id : "heading"}, "Hello from React !!!");
 //createElement ==>  h1 => tag name, {} => tag attribute, "_______" => children / content 
 const root1 = ReactDOM.createRoot(document.getElementById("root"));
 //root1.render(heading1);
@@ -21,7 +24,7 @@ const root1 = ReactDOM.createRoot(document.getElementById("root"));
 const parent = React.createElement("div", {id : "parent"}, [ 
   React.createElement("div", {id : "child1"}, [
     React.createElement("h1", {}, "Im h1 tag"),
-    React.createElement("h2", {}, "Im h2 tag")
+    React.createElement("h2", {}, "This is coming from react")
   ]),
   React.createElement("div", {id : "child2"}, [
     React.createElement("h1", {}, "Im h1 tag"),
