fe545a5594595513e025cedf893359f8baa62004
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-03 13:09:00 +0530

    MODIFIED: created React Element using JSX

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", {id : "heading"}, "Hello from React !!!");
-//createElement ==>  h1 => tag name, {} => tag attribute, "_______" => children / content 
-const root1 = ReactDOM.createRoot(document.getElementById("root"));
+/*
+const heading1 = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello from React !!!"
+);
+*/
+//createElement ==>  h1 => tag name, {} => tag attribute, "_______" => children / content
+const root = ReactDOM.createRoot(document.getElementById("root"));
 //root1.render(heading1);
 
 /**
- * 
+ *
  * <div id="parent">
  *   <div id="child1">
  *     <h1>Im h1 tag</h1>
@@ -18,18 +24,33 @@ const root1 = ReactDOM.createRoot(document.getElementById("root"));
  *     <h2>Im h2 tag</h2>
  *   </div>
  * </div>
- * 
+ *
  */
 
-const parent = React.createElement("div", {id : "parent"}, [ 
-  React.createElement("div", {id : "child1"}, [
+/*
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "Im h1 tag"),
-    React.createElement("h2", {}, "This is coming from react")
+    React.createElement("h2", {}, "This is coming from react"),
   ]),
-  React.createElement("div", {id : "child2"}, [
+  React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "Im h1 tag"),
-    React.createElement("h2", {}, "Im h2 tag")
+    React.createElement("h2", {}, "Im h2 tag"),
   ]),
-])
+]);
+*/
 
-root1.render(parent)
+const parent = (
+  <>
+    <div id="child1">
+      <h1>Im h1 tag</h1>
+      <h1>This is coming from react</h1>
+    </div>
+    <div id="child2">
+      <h1>Im h1 tag</h1>
+      <h2>Im h2 tag</h2>
+    </div>
+  </>
+);
+
+root.render(parent);
